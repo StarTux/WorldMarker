18412f6e3bea0fea80f60d186c41943f8f271bee
StarTux <ths.loy@googlemail.com>
2021-04-26 19:05:29 +0200

    Restore frozen item events when not broken by player
    Frozen item ids were not restored when dropped from a block with tile
    entity under the following conditions:
    - Water flow
    - Piston extension
    - Block explosion
    - Entity explosion
    Now they will be.

--- src/main/java/com/cavetale/worldmarker/EventListener.java
+++ src/main/java/com/cavetale/worldmarker/EventListener.java
@@ -2,15 +2,25 @@ package com.cavetale.worldmarker;
 
 import com.cavetale.worldmarker.block.BlockMarker;
 import com.cavetale.worldmarker.item.ItemMarker;
+import com.cavetale.worldmarker.util.Util;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.TileState;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDropItemEvent;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.block.BlockFromToEvent;
+import org.bukkit.event.block.BlockPistonExtendEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.world.ChunkUnloadEvent;
@@ -26,6 +36,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 @RequiredArgsConstructor
 final class EventListener implements Listener {
     private final WorldMarkerPlugin plugin;
+    private Map<Block, String> poppedBlocks = new HashMap<>();
 
     public void enable() {
         Bukkit.getPluginManager().registerEvents(this, plugin);
@@ -64,9 +75,61 @@ final class EventListener implements Listener {
         PersistentDataContainer tag = tile.getPersistentDataContainer();
         if (!tag.has(WorldMarkerPlugin.ID_KEY, PersistentDataType.STRING)) return;
         String id = tag.get(WorldMarkerPlugin.ID_KEY, PersistentDataType.STRING);
-        if (event.getItems().size() != 1) return;
+        if (event.getItems().size() != 1) return; // necessary?
         ItemStack item = event.getItems().get(0).getItemStack();
         if (ItemMarker.hasId(item)) return;
+        ItemMarker.setId(item, id);
+    }
+
+    private void onBlockPop(final Block block) {
+        BlockState state = block.getState();
+        if (!(state instanceof TileState)) return;
+        TileState tile = (TileState) state;
+        PersistentDataContainer tag = tile.getPersistentDataContainer();
+        if (!tag.has(WorldMarkerPlugin.ID_KEY, PersistentDataType.STRING)) return;
+        String id = tag.get(WorldMarkerPlugin.ID_KEY, PersistentDataType.STRING);
+        poppedBlocks.put(block, id);
+        Bukkit.getScheduler().runTask(plugin, () -> {
+                String id2 = poppedBlocks.remove(block);
+                if (id2 != null) {
+                    plugin.getLogger().warning(id + ", " + id2 + ": unused popped block: " + Util.toString(block));
+                }
+            });
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    void onBlockFromTo(BlockFromToEvent event) {
+        if (event.getBlock().getType() != Material.WATER) return;
+        onBlockPop(event.getToBlock());
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    void onBlockPistonExtend(BlockPistonExtendEvent event) {
+        for (Block block : event.getBlocks()) {
+            onBlockPop(block);
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    void onBlockExplode(BlockExplodeEvent event) {
+        for (Block block : event.blockList()) {
+            onBlockPop(block);
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    void onEntityExplode(EntityExplodeEvent event) {
+        for (Block block : event.blockList()) {
+            onBlockPop(block);
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    void onItemSpawn(ItemSpawnEvent event) {
+        String id = poppedBlocks.remove(event.getLocation().getBlock());
+        if (id == null) return;
+        ItemStack item = event.getEntity().getItemStack();
+        if (item.getAmount() != 1) return; // necessary?
         ItemMarker.setId(item, id);
     }
 
