48522c329e68efc87104b31718ae72e2c110dfdb
StarTux <ths.loy@googlemail.com>
2021-10-07 19:46:29 +0200

    lower-case

--- src/main/java/com/cavetale/worldmarker/WorldMarkerPlugin.java
+++ src/main/java/com/cavetale/worldmarker/WorldMarkerPlugin.java
@@ -7,7 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class WorldMarkerPlugin extends JavaPlugin {
     @Getter static WorldMarkerPlugin instance;
-    @SuppressWarnings("Deprecation")
+    @SuppressWarnings("deprecation")
     public static final NamespacedKey ID_KEY = new NamespacedKey("worldmarker", "id");
     final WorldMarkerCommand command = new WorldMarkerCommand(this);
     final EventListener eventListener = new EventListener(this);
