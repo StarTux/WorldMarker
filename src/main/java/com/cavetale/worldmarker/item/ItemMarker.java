8ef55c50afeb2491359702265748dc8beba98015
StarTux <ths.loy@gmail.com>
2025-05-19 14:22:14 +0200

    Use PersistentDataContainer API in ItemStack

--- src/main/java/com/cavetale/worldmarker/item/ItemMarker.java
+++ src/main/java/com/cavetale/worldmarker/item/ItemMarker.java
@@ -1,6 +1,8 @@
 package com.cavetale.worldmarker.item;
 
 import com.cavetale.worldmarker.WorldMarkerPlugin;
+import io.papermc.paper.persistence.PersistentDataContainerView;
+import java.util.Objects;
 import lombok.NonNull;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -11,10 +13,9 @@ public final class ItemMarker {
     private ItemMarker() { }
 
     public static void setId(@NonNull ItemStack item, @NonNull String id) {
-        ItemMeta meta = item.getItemMeta();
-        PersistentDataContainer tag = meta.getPersistentDataContainer();
-        tag.set(WorldMarkerPlugin.ID_KEY, PersistentDataType.STRING, id);
-        item.setItemMeta(meta);
+        item.editPersistentDataContainer(tag -> {
+                tag.set(WorldMarkerPlugin.ID_KEY, PersistentDataType.STRING, id);
+            });
     }
 
     public static void setId(@NonNull ItemMeta meta, @NonNull String id) {
@@ -23,27 +24,22 @@ public final class ItemMarker {
     }
 
     public static void resetId(@NonNull ItemStack item) {
-        ItemMeta meta = item.getItemMeta();
-        PersistentDataContainer tag = meta.getPersistentDataContainer();
-        tag.remove(WorldMarkerPlugin.ID_KEY);
-        item.setItemMeta(meta);
+        item.editPersistentDataContainer(tag -> {
+                tag.remove(WorldMarkerPlugin.ID_KEY);
+            });
     }
 
     public static String getId(@NonNull ItemStack item) {
-        if (!item.hasItemMeta()) return null;
-        ItemMeta meta = item.getItemMeta();
-        PersistentDataContainer tag = meta.getPersistentDataContainer();
-        if (!tag.has(WorldMarkerPlugin.ID_KEY, PersistentDataType.STRING)) return null;
-        return tag.get(WorldMarkerPlugin.ID_KEY, PersistentDataType.STRING);
+        final PersistentDataContainerView tag = item.getPersistentDataContainer();
+        if (tag == null) return null; // marked @NotNull but still
+        return tag.getOrDefault(WorldMarkerPlugin.ID_KEY, PersistentDataType.STRING, null);
     }
 
     public static boolean hasId(@NonNull ItemStack item) {
-        String itemId = getId(item);
-        return itemId != null;
+        return getId(item) != null;
     }
 
     public static boolean hasId(@NonNull ItemStack item, @NonNull String id) {
-        String itemId = getId(item);
-        return itemId != null && itemId.equals(id);
+        return Objects.equals(getId(item), id);
     }
 }
